ae6564de5badea155cbb03ad8d19d624330be39f
ekram <ekruiwc2020@gmail.com>
2024-03-24 09:16:14 +0300

    feat: initial commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+
+const Index = () => {
+  return (
+    <div className=""> 
+      WELCOME
+    </div>  
+  );
+};
+
+
+export default Index;
