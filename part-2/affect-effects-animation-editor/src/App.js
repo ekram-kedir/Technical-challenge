fce93ca1f6cf4209bb3c57b9b3d5869412ea0a8f
ekram <ekruiwc2020@gmail.com>
2024-03-25 19:45:17 +0300

    refactor: import necessary components

--- part-2/affect-effects-animation-editor/src/App.js
+++ part-2/affect-effects-animation-editor/src/App.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import CommonAncestor from './components/CommonAncestorComponent';
 import EditorPlayGround from './components/EditorPlayGround';
 import ImportAnimation from './components/ImportAnimation';
+import ColorEditor from './components/ColorEditor';
+
 export default function App() {
 
   return (
     <div>
       <EditorPlayGround />
-      <CommonAncestor />
+      {/* <CommonAncestor /> */}
+      {/* <ColorEditor /> */}
     </div>
   );
 }
